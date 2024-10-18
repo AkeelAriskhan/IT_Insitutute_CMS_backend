3459173b15439b02e9a476ca8250145b64d1e982
Jenittah Suthakaran <iamjenittahs@gmail.com>
2024-10-18 09:12:55 +0530

    student repo

---
+++ IT_Insitutute_CMS/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IT_Insitutute_CMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IstudentRepository _istudentRepository;
+
+        public studentcontroller(IstudentRepository istudentRepository)
+        {
+            _istudentRepository = istudentRepository;
+        }
+
+    }
+}
