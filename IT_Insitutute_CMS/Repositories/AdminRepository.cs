4431eb1745083f44ed623b29178dc5970bd62601
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 08:06:18 +0530

    Add student rowsql

--- IT_Insitutute_CMS/Repositories/AdminRepository.cs
+++ IT_Insitutute_CMS/Repositories/AdminRepository.cs
@@ -1,4 +1,6 @@
-﻿using IT_Insitutute_CMS.IRepositories;
+﻿using IT_Insitutute_CMS.Entities;
+using IT_Insitutute_CMS.IRepositories;
+using Microsoft.Data.Sqlite;
 
 namespace IT_Insitutute_CMS.Repositories
 {
@@ -9,6 +11,24 @@ namespace IT_Insitutute_CMS.Repositories
         public AdminRepository(string connectionString)
         {
             _connectionString = connectionString;
+        }
+        public void AddStudent(Student student)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO Students(Nic,FullName,Email,PhoneNumber,Password,RegistrationFee,ImagePath) VALUES(@Nic,@FullName,@Email,@PhoneNumber,@Password,@RegistrationFee,@imagePath)";
+                command.Parameters.AddWithValue("@Nic", student.Nic);
+                command.Parameters.AddWithValue("@FullName", student.FullName);
+                command.Parameters.AddWithValue("@Email", student.Email);
+                command.Parameters.AddWithValue("@PhoneNumber", student.PhoneNumber);
+                command.Parameters.AddWithValue("@Password", student.Password);
+                command.Parameters.AddWithValue("@RegistrationFee", student.RegistrationFee);
+                command.Parameters.AddWithValue("@imagePath", student.ImagePath == null ? "" : student.ImagePath);
+                command.ExecuteNonQuery();
+            }
+
         }
     }
 }
