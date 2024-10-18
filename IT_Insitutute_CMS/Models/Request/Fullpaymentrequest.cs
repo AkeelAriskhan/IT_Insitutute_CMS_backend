dc111def27674600b0408dcf594fc8a4637756fd
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:06:07 +0530

    Fullpaymentrequest model

---
+++ IT_Insitutute_CMS/Models/Request/Fullpaymentrequest.cs
@@ -0,0 +1,8 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class Fullpaymentrequest
+    {
+        public string Nic { get; set; }
+        public decimal Fullpaymentamount { get; set; }
+    }
+}
