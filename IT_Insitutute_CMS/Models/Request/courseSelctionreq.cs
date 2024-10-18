aa513151c254e2a9ee5e1008686b6884ed7f2a6f
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:05:04 +0530

    courseSelctionreq model

---
+++ IT_Insitutute_CMS/Models/Request/courseSelctionreq.cs
@@ -0,0 +1,11 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class courseSelctionreq
+    {
+        public string course { get; set; }
+
+        public string ProficiencyLevels { get; set; }
+
+        public string duration { get; set; }
+    }
+}
