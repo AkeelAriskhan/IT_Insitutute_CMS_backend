58c27de52dd36522278d50949e7bad3a15f223b3
Joel-1641 <ut01641tic2024@gmail.com@gmail.com>
2024-10-18 11:34:23 +0530

    ContactUS method Created

---
+++ IT_Insitutute_CMS/Controllers/ContactUsController.cs
@@ -0,0 +1,27 @@
+﻿using IT_Insitutute_CMS.Entities;
+using IT_Insitutute_CMS.IRepositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IT_Insitutute_CMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactUsController : ControllerBase
+    {
+        private readonly IContactUsRepository _contactUsRepository;
+
+        public ContactUsController(IContactUsRepository contactUsRepository)
+        {
+            _contactUsRepository = contactUsRepository;
+        }
+
+        [HttpPost("Add-ContactUs-Details")]
+
+        public IActionResult AddContactUsDetails(ContactUs contactUs)
+        {
+            var ContactUsDetails = _contactUsRepository.AddContactUsDetails(contactUs);
+            return Ok(ContactUsDetails);
+        }
+    }
+}
