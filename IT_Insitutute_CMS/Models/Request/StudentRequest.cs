5ec8bf59db97a4b23a34a7c0ec779414c21fb32b
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:10:08 +0530

    StudentRequest model

---
+++ IT_Insitutute_CMS/Models/Request/StudentRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class StudentRequest
+    {
+        public string Nic { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public decimal RegistrationFee { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
