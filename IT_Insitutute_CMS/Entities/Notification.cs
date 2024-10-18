1e9f039be1e457bf832a43b25ede13e8cfb7ba64
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:02:41 +0530

    Notificationevtity done

---
+++ IT_Insitutute_CMS/Entities/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_Insitutute_CMS.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Nic { get; set; }
+        public string Type { get; set; }
+        public string SourceId { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
