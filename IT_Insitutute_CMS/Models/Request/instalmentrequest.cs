841558525006a47edddd85ce56da3cd50a65e56f
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:07:09 +0530

    instalmentrequest model

---
+++ IT_Insitutute_CMS/Models/Request/instalmentrequest.cs
@@ -0,0 +1,12 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class instalmentrequest
+    {
+        public string Nic { get; set; }
+        public decimal installmentAmount { get; set; }
+        public string Installments { get; set; }
+        public decimal paymentDue { get; set; }
+        public decimal PaymentPaid { get; set; }
+        public decimal totalAmount { get; set; }
+    }
+}
