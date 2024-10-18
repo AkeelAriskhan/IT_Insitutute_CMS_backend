d032128bf4b7bcf42a12cddb8eefa91841f83e48
Jenittah Suthakaran <iamjenittahs@gmail.com>
2024-10-18 09:16:09 +0530

    Course selection

--- IT_Insitutute_CMS/Repositories/StudentRepository.cs
+++ IT_Insitutute_CMS/Repositories/StudentRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace IT_Insitutute_CMS.Repositories
+﻿using IT_Insitutute_CMS.Models.Request;
+using Microsoft.Data.Sqlite;
+
+namespace IT_Insitutute_CMS.Repositories
 {
     public class StudentRepository: IstudentRepository
     {
@@ -6,6 +9,21 @@
         public studentRepository(string connectionString)
         {
             _connectionString = connectionString;
+        }
+        public void courseselction(string nic, courseSelctionreq coursesel)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "UPDATE Students SET course=@course ,ProficiencyLevels=@ProficiencyLevels,duration=@duration WHERE Nic == @Nic";
+                command.Parameters.AddWithValue("@Nic", nic);
+                command.Parameters.AddWithValue("@course", coursesel.course);
+                command.Parameters.AddWithValue("@ProficiencyLevels", coursesel.ProficiencyLevels);
+                command.Parameters.AddWithValue("@duration", coursesel.duration);
+                command.ExecuteNonQuery();
+            }
+
         }
     }
 }
