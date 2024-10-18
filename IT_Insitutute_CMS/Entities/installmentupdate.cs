2c10ae6c3f9f1fdd73480d1496de31ca1ed8a5dc
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:01:30 +0530

    installmentupdate entity done

---
+++ IT_Insitutute_CMS/Entities/installmentupdate.cs
@@ -0,0 +1,8 @@
+﻿namespace IT_Insitutute_CMS.Entities
+{
+    public class installmentupdate
+    {
+        public string Nic { get; set; }
+        public decimal installmentAmount { get; set; }
+    }
+}
