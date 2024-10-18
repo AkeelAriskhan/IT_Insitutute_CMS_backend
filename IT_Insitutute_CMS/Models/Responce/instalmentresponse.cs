62628c61f51723832aa6c649edc10b984992419b
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:12:31 +0530

    instalmentresponse model

---
+++ IT_Insitutute_CMS/Models/Responce/instalmentresponse.cs
@@ -0,0 +1,15 @@
+﻿namespace IT_Insitutute_CMS.Models.Responce
+{
+    public class instalmentresponse
+    {
+        public string Nic { get; set; }
+        public decimal installmentAmount { get; set; }
+        public string Installments { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal paymentDue { get; set; }
+
+
+        public decimal PaymentPaid { get; set; }
+        public decimal totalAmount { get; set; }
+    }
+}
