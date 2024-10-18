515841a546886e4617b856d755fc0d32ffcacb0b
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:14:38 +0530

    regstuResponce model

---
+++ IT_Insitutute_CMS/Models/Responce/regstuResponce.cs
@@ -0,0 +1,22 @@
+﻿namespace IT_Insitutute_CMS.Models.Responce
+{
+    public class regstuResponce
+    {
+        public string Nic { get; set; }
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+        public decimal RegistrationFee { get; set; }
+        public string? ImagePath { get; set; }
+        public string course { get; set; }
+
+        public string ProficiencyLevels { get; set; }
+
+        public string duration { get; set; }
+        public decimal fullpayment { get; set; }
+        public DateTime paymentDate { get; set; }
+    }
+}
