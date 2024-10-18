cb2f01ac349aeeb8250ceba28c53f2eaac8b002d
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:09:14 +0530

    PaymentRequest model

---
+++ IT_Insitutute_CMS/Models/Request/PaymentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class PaymentRequest
+    {
+        public string Nic { get; set; }
+        public int courseFee { get; set; }
+        public string paymentPlans { get; set; }
+        public int totalamount { get; set; }
+        public string Installments { get; set; }
+    }
+}
