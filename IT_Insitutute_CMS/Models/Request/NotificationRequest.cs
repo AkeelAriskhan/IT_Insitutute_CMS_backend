562c387b3d9c2e3afcb796064856aa5a664c9ad2
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-18 07:08:21 +0530

    NotificationRequest model

---
+++ IT_Insitutute_CMS/Models/Request/NotificationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace IT_Insitutute_CMS.Models.Request
+{
+    public class NotificationRequest
+    {
+        public string Nic { get; set; }
+        public string Type { get; set; }
+        public string SourceId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
