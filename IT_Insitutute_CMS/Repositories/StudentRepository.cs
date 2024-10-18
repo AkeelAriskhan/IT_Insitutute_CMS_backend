8e3319fa198745cf04330a94b002c7f7220ee008
Jenittah Suthakaran <iamjenittahs@gmail.com>
2024-10-18 09:15:37 +0530

    student connection string

---
+++ IT_Insitutute_CMS/Repositories/StudentRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace IT_Insitutute_CMS.Repositories
+{
+    public class StudentRepository: IstudentRepository
+    {
+        private readonly string _connectionString;
+        public studentRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+}
