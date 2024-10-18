694e683aad9f18bda4ade5942a54324db44d6e9d
Jenittah Suthakaran <iamjenittahs@gmail.com>
2024-10-18 10:01:53 +0530

    2c

--- IT_Insitutute_CMS/Repositories/PaymentsRepository.cs
+++ IT_Insitutute_CMS/Repositories/PaymentsRepository.cs
@@ -1,6 +1,51 @@
-﻿namespace IT_Insitutute_CMS.Repositories
+﻿using IT_Insitutute_CMS.IRepositories;
+using IT_Insitutute_CMS.Models.Responce;
+using Microsoft.Data.Sqlite;
+
+namespace IT_Insitutute_CMS.Repositories
 {
-    public class PaymentsRepository
+    public class PaymentsRepository: IPaymentRepository
     {
+        private readonly string _connectionString;
+        public PaymentsRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public instalmentresponse getinsallmentbydetailsbynic(string nic)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var coommand = connection.CreateCommand();
+                coommand.CommandText = @"select* from installment where Nic=@Nic";
+                coommand.Parameters.AddWithValue("@Nic", nic);
+                using (var reader = coommand.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var intallment = new instalmentresponse()
+                        {
+                            Nic = reader.GetString(0),
+                            installmentAmount = reader.GetDecimal(1),
+                            Installments = reader.GetString(2),
+                            PaymentDate = reader.GetDateTime(3),
+                            paymentDue = reader.GetDecimal(4),
+                            PaymentPaid = reader.GetDecimal(5),
+                            totalAmount = reader.GetDecimal(6),
+                        };
+                        return intallment;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
+                }
+            }
+
+
+        }
+
     }
 }
